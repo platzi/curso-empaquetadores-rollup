f89c268ae44529908e6d3c68c1f31312ab78fcbb
Oscar Barajas <oscar@gndx.dev>
2022-06-22 17:09:50 -0500

    :rocket: [ADD] rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,28 @@
+import html from '@rollup/plugin-html';
+import copy from 'rollup-plugin-copy';
+import css from 'rollup-plugin-css-only';
+import image from '@rollup/plugin-image';
+
 export default {
   input: "src/index.js",
   output: {
     file: 'dist/bundle.js',
     format: 'es',
     sourcemap: true,
-  }
-  
+  },
+  plugins: [
+    copy({
+      targets: [
+        {
+          src: "src/assets/**/*",
+          dest: "dist/asset"
+        }
+      ]
+    }),
+    css({
+      output: 'bundle.css'
+    }),
+    html(),
+    image(),
+  ]
 }
